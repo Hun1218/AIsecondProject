c09c2cd65fede0cfe75cacc0a7b7bf562a5ee550
sping-coding <parkin153@naver.com>
2022-11-17 15:58:00 +0900

    '실험'

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
         <Route path="/my" element={<Mypage />} />
         <Route path="/findID" element={<FindID />} />
         <Route path="/findPW" element={<FindPW />} />
+        console.log('hi');
       </Routes>
     </Router>
   );
